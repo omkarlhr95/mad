ec28b0355767c003d43206043b8d47ee3325bf4e
omkarlhr95 <omkarlhr95@gmail.com>
2025-11-20 11:07:57 +0530

    Delete selenium_practical directory

--- selenium_practical/type_webele.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-driver=webdriver.Chrome()
-driver.get("https://www.google.com/")
-single_webel=driver.find_element(By.XPATH,"//textarea")
-multi_webel=driver.find_elements(By.XPATH,"//textarea")
-print(type(single_webel))
-print(type(multi_webel))
